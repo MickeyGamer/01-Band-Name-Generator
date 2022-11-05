ef2dde532ae758b7991cb43bb04a385e11b9ec2a
MickeyGamer <64866988+MickeyGamer@users.noreply.github.com>
2022-11-05 13:56:05 -0700

    Add files via upload

---
+++ 01 Band Name Generrator.py
@@ -0,0 +1,6 @@
+# This is a fun generator where you basically combine two inputs and generate your bandname!
+print("Welcome to the Band Generator!\n")
+a = input("What is the name of the city you grew up in?")
+b = input("What's your pet name?")
+
+print("Your band name could be:- " + a + " " + b)
